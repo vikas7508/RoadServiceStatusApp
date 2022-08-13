4c31b08c65bec83a83fa1f97b505aaf15a0fa14a
Vikas Kharb <vikas.kharb@tietoevry.com>
2022-08-13 17:22:14 +0200

    Adding Test Cases For the Project

---
+++ TFLRoadStatusApp/TFLRoadStatusApp.Test/Helpers/TflRoadMessageHelperTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TflRoadStatusApp.Helpers;
+using TflRoadStatusApp.Modal.enums;
+
+namespace TflRoadStatusApp.Test.Helpers
+{
+    [TestClass]
+    public class TflRoadMessageHelperTest
+    {         
+        TflRoadMessageHelper printSvc;
+        [TestInitialize]
+        public void TflRoadMessageHelperTest_Init()
+        { 
+            printSvc = new TflRoadMessageHelper();
+        }       
+
+        [TestMethod]
+        public void Test_PrintInvalidRoadMessage()
+        {
+            int returnValue = printSvc.PrintInvalidRoadMessage("{}"); 
+            Assert.AreEqual(returnValue, (int)ExitCode.InvalidRoad);
+        }
+
+        [TestMethod]
+        public void Test_PrintSuccessMessage()
+        {
+            int returnValue = printSvc.PrintSuccessMessage("[{}]");             
+            Assert.AreEqual(returnValue, (int)ExitCode.Success);
+        }
+    }
+
+    
+}
---
+++ TFLRoadStatusApp/TFLRoadStatusApp.Test/TflRoadAppMainTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TflRoadStatusApp.Helpers;
+using TflRoadStatusApp.Helpers.Interfaces;
+using TflRoadStatusApp.Modal.enums;
+
+namespace TflRoadStatusApp.Test
+{
+    [TestClass]
+    public class TflRoadAppMainTest
+    {
+        TflRoadAppMain mainApp;
+        Mock<ITflRoadMessageHelper> messageHelperMock;
+        ILogger<TflRoadAppMain> loggerMock;
+
+        [TestInitialize]
+        public void TflRoadAppMainTest_Init()
+        {
+            loggerMock = Mock.Of<ILogger<TflRoadAppMain>>();
+            messageHelperMock = new Mock<ITflRoadMessageHelper>();
+            messageHelperMock.Setup(x => x.Print(It.IsAny<string>()));
+            messageHelperMock.Setup(x => x.Read()).Returns("");
+            messageHelperMock.Setup(x => x.PrintSuccessMessage(It.IsAny<string>())).Returns((int)ExitCode.Success);
+            messageHelperMock.Setup(x => x.PrintInvalidRoadMessage(It.IsAny<string>())).Returns((int)ExitCode.InvalidRoad);
+        }
+        [TestMethod]
+        public void Test_RunAsync_ForSuccessResponse()
+        {
+            var svcMock = new Mock<ITflRoadService>();
+            svcMock.Setup(x => x.Get(It.IsAny<string>()))
+                .Returns(Task.FromResult(new HttpResponseMessage()
+                    { Content = new StringContent("[{}]"), StatusCode = System.Net.HttpStatusCode.OK }));
+
+            mainApp = new TflRoadAppMain(svcMock.Object, loggerMock, messageHelperMock.Object);
+            var result = mainApp.RunAsync(Array.Empty<string>()).Result;
+            Assert.AreEqual((int)ExitCode.Success, result);
+        }
+
+        [TestMethod]
+        public void Test_RunAsync_ForInvalidResponse()
+        {
+           
+            var svcMock = new Mock<ITflRoadService>();
+            svcMock.Setup(x => x.Get(It.IsAny<string>()))
+                .Returns(Task.FromResult(new HttpResponseMessage()
+                { Content = new StringContent("[{}]"), StatusCode = System.Net.HttpStatusCode.NotFound }));
+
+            mainApp = new TflRoadAppMain(svcMock.Object, loggerMock, messageHelperMock.Object);
+            var result = mainApp.RunAsync(Array.Empty<string>()).Result;
+            Assert.AreEqual((int)ExitCode.InvalidRoad, result);
+        }
+
+        [TestMethod]
+        public void Test_RunAsync_ForExceptionCheck()
+        {
+            var svcMock = new Mock<ITflRoadService>();
+            svcMock.Setup(x => x.Get(It.IsAny<string>()))
+                .Returns(()=>null);
+
+            mainApp = new TflRoadAppMain(svcMock.Object, loggerMock, messageHelperMock.Object);
+            var result = mainApp.RunAsync(Array.Empty<string>()).Result;
+            Assert.AreEqual((int)ExitCode.UnknownError, result);
+        }
+
+    }
+}
